778e7fadd527dc0132e6a1feaf570bbe377b02d8
mahad-debug <102515712+mahad-debug@users.noreply.github.com>
2023-08-29 16:54:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,39 @@
-const express = require("express");
-var cors = require("cors");
-const { Builder, By, until } = require("selenium-webdriver");
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
-const app = express();
-app.use(cors());
-const port = 3000;
+// Set up Chrome options in headless mode
+const chromeOptions = new chrome.Options();
+chromeOptions.addArguments("--headless");
 
-app.get("/api/follow-company", async (req, res) => {
+async function followLinkedInCompany() {
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(chromeOptions)
+    .build();
+
   try {
-    console.log("Hit api");
-
-    const driver = await new Builder().forBrowser("chrome").build();
-
-    // Define the Company URL (replace with the actual LinkedIn company URL)
+    // Define the LinkedIn company URL you want to follow
     const companyUrl = "https://www.linkedin.com/company/nestle-s-a-";
 
-    // Open the Company URL in the same tab
+    // Open the LinkedIn company page
     await driver.get(companyUrl);
 
-    // Rest of your automation code here
-
-    // Quit the WebDriver when done
-    // Wait for follow button to be clickable
-    let followButton = await driver.wait(
-      until.elementLocated(By.css(".follow"))
+    // Wait for the follow button to be clickable
+    const followButton = await driver.wait(
+      until.elementLocated(By.css(".follow-button")),
+      10000 // Adjust the timeout as needed
     );
-
-    // await driver.sleep(5000);
 
     // Click the follow button
     await followButton.click();
 
-    // Wait for button click to register
-    await driver.sleep(20000);
-
+    console.log("Company followed successfully!");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
     // Close the browser
     await driver.quit();
-
-    // Send a response back to the client (HTML page)
-    res.json({ message: "Company followed successfully!" });
-  } catch (err) {
-    // Handle errors
-    console.error("An error occurred:", err);
-    res.status(500).json({ error: "An error occurred" });
   }
-});
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-//////////////
+followLinkedInCompany();
